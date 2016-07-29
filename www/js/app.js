14d036dc9b107fac42119c573de9b185a165ac0b
Francisco André <fandrefh@gmail.com>
2016-07-29 12:06:22 -0300

    Primeiro commit

---
+++ www/js/app.js
@@ -0,0 +1,26 @@
+$('.collection').on('click', '.collection-item', function(){
+	var $badge = $('.badge', this);
+	if ($badge.length === 0) {
+		$badge = $('<span class="badge brown-text">0</span>').appendTo(this);
+	}
+	$badge.text(parseInt($badge.text()) + 1);
+
+	var nomeDoProduto = this.firstChild.textContent;
+    Materialize.toast(nomeDoProduto + ' adicionado!', 1000);
+});
+
+$('.modal-trigger').leanModal();
+
+$('#confirmar').on('click', function () {
+	var texto = '';
+	$('.badge').parent().each(function() {
+		texto += this.firstChild.textContent + ': ';
+		texto += this.lastChild.textContent + ', ';
+	});
+	$('#resumo').empty().text(texto);
+});
+
+$('.collection').on('click', '.badge', function() {
+	$(this).remove();
+	return false;
+});
