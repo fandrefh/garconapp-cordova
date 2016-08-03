1fe7fcbf43686b5fc1cdce17bc06c3639daba91c
Francisco André <fandrefh@gmail.com>
2016-08-03 08:14:46 -0300

    Leitor de QR Code e StatusBar

--- www/js/app.js
+++ www/js/app.js
@@ -24,3 +24,35 @@ $('.collection').on('click', '.badge', function() {
 	$(this).remove();
 	return false;
 });
+
+$('.scan-qrcode').on('click', function(){
+	cordova.plugins.barcodeScanner.scan(
+		function (resultado) {
+			if (resultado.text) {
+				Materialize.toast('Mesa: ' + resultado.text, 2000);
+				$('#numero-mesa').val(resultado.text);
+			}
+		},
+		function (error) {
+			Materialize.toast('Error: ' + error, 3000, 'red-text');
+		}
+	);
+});
+
+$('.acao-finalizar').on('click', function () {
+	$.ajax({
+		url: 'http://cozinhapp.sergiolopes.org/novo-pedido',
+		data: {
+			mesa: $('#numero-mesa').val(),
+			pedido: $('#resumo').text()
+		},
+		error: function(error) {
+			Materialize.toast(error.responseText, 3000, 'red-text');
+		},
+		success: function (dados) {
+			Materialize.toast(dados, 2000);
+			$('#numero-mesa').val('');
+			$('.badge').remove();
+		}
+	});
+});
